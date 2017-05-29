bf82fac140b93c1ee5f26e0d1e8fbd24f5984d39
Lisa Mishra <lisamishra94@gmail.com>
2017-05-28 20:26:14 -0500

    Add files via upload

---
+++ assets/javascript/game.js
@@ -0,0 +1,105 @@
+$(document).ready(function () {
+
+	var numbersArray = [];
+	var wins = 0;
+	var loss = 0;
+	var currentScore = 0;
+
+// Matched number
+
+	var matchNum = Math.floor(Math.random() * 102 + 19)
+	$("#match").text(" " +matchNum);
+
+
+// Crystal Numbers
+
+	function crystalNum() {
+		for (var i = 0; i < 4; i++) {
+			var x = Math.floor(Math.random() * 12 + 1);
+			numbersArray.push(x);
+		}
+
+	}
+
+crystalNum();	
+
+	
+// Crystal Functions
+
+	$("#blue").on ('click', function () {
+		currentScore = currentScore + numbersArray[0];
+		$("#score").text(" " + currentScore)
+
+		if (currentScore == matchNum) {
+			winCount();
+		}
+
+		else if (currentScore > matchNum) {
+			lossCount();
+		}
+	})
+
+	$("#green").on ('click', function () {
+		currentScore = currentScore + numbersArray[1];
+		$("#score").text(" " + currentScore)
+
+		if (currentScore == matchNum) {
+			winCount();
+		}
+
+		else if (currentScore > matchNum) {
+			lossCount();
+		}
+	})
+
+	$("#orange").on ('click', function () {
+		currentScore = currentScore + numbersArray[2];
+		$("#score").text(" " + currentScore)
+
+		if (currentScore == matchNum) {
+			winCount();
+		}
+
+		else if (currentScore > matchNum) {
+			lossCount();
+		}
+	})
+
+	$("#purple").on ('click', function () {
+		currentScore = currentScore + numbersArray[3];
+		$("#score").text(" " + currentScore)
+
+		if (currentScore == matchNum) {
+			winCountw();
+		}
+
+		else if (currentScore > matchNum) {
+			lossCount();
+		}
+	})
+
+	
+//Win Count
+	function winCount(){
+		wins++;
+		$("#wins").text(" " + wins)
+		reset()
+	}
+
+// Loss Count
+	function lossCount(){
+		loss++;
+		$("#loss").text(" " + loss)
+		reset()
+	}
+	
+	function reset () {
+		numbersArray = [];
+		matchNum = Math.floor(Math.random() * 102 + 19)
+		crystalNum ();
+		$("#match").text(" " +matchNum);
+		currentScore = 0;
+		$("#score").text(" " + currentScore);
+	}
+
+});
